3c0a79d5f53127a30a029e276f45cf7a7c34991b
Zzhiwei <zhiwei3141@gmail.com>
2024-09-29 00:47:15 +0800

    Task I: Logging In - Adds users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_23_012329) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_28_162157) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -42,6 +42,13 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_23_012329) do
     t.text "description"
     t.string "image_url"
     t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ test.rb
@@ -0,0 +1,9 @@
+class MyClass
+  # Define a class method using self
+  def self.my_class_method
+    puts "Hello from the class method!"
+  end
+
+  # Call the class method within the class definition
+  my_class_method
+end
